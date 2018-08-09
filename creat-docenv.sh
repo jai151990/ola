8ade29b3c6b75d96318845ec9cd12ca8de87bfbd
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-08-09 00:48:10 -0700

    Update creat-docenv.sh

--- creat-docenv.sh
+++ creat-docenv.sh
@@ -6,6 +6,6 @@ do
   echo "container creating siva$i"
   docker run -it -d siva$i --rm siva-img /bin/bash
   echo "siva$i container created"
-  echo "=========================================
+  echo "========================================="
 done
 docker inspect -f '{{.NetworkSettings.Networks.bridge.IPAddress}}' `docker ps -q` > ip.txt
