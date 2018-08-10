de234dff65ac170640f61085c4c412bffe66eb45
shankar <jaisiva151990@gmail.com>
2018-08-09 21:24:48 -0700

    all file committed

--- creat-docenv.sh
+++ creat-docenv.sh
@@ -4,6 +4,8 @@ for i in `seq $container_count`
 do
   echo "========================================="
   echo "container creating siva$i"
-  docker run -it -d siva$i --rm siva-img /bin/bash
+  docker run -it -d --name siva$i --rm jaisriram111/siva-img /bin/bash
 done
 echo "siva$i container created"
+
+docker inspect -f {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > ip.txt
